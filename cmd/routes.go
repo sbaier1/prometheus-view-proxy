f13ba6c9e47f6210f744292245d7d3e5036cc39e
Simon Baier <simonbaier@outlook.de>
2020-12-09 00:28:46 +0100

    initial, untested, rough prometheus api and gotemplate integration

--- cmd/routes.go
+++ cmd/routes.go
@@ -4,24 +4,49 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"text/template"
+	"time"
+
+	"github.com/prometheus/client_golang/api"
+	prometheus "github.com/prometheus/client_golang/api/prometheus/v1"
 )
 
 type templateHandler struct {
 	query    string
 	template string
+	client   prometheus.API
 }
 
 func (th *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	//tm := time.Now().Format(th.format)
+	data, warn, err := th.client.Query(r.Context(), th.query, time.Now())
+	if warn != nil {
+		log.Printf("Warnings emitted from query %s: %v", th.query, warn)
+	}
+	if err != nil {
+		log.Fatalf("Error emitted from query %s: %v", th.query, err)
+	}
+	t, err := template.New(th.query).Parse(th.template)
+	if err != nil {
+		log.Fatalf("Failed to parse template: %s: %v", th.template, err)
+	}
+	err = t.Execute(w, data)
+	if err != nil {
+		log.Fatalf("Failed to execute template for data %s: %v", data, err)
+	}
 	w.Write([]byte("The time is: " + th.query))
 }
 
 // NewRoutes generates the HTTP handler for the proxy
 func NewRoutes(upstream *url.URL, config Config) http.Handler {
 	mux := http.NewServeMux()
+	client, err := api.NewClient(api.Config{Address: upstream.String()})
+	if err != nil {
+		log.Fatalf("Failed to initialize Prometheus API client %v", err)
+	}
+	apiClient := prometheus.NewAPI(client)
 	for path, cfg := range config.Routes {
 		log.Printf("Serving query '%s' at route '/%s'", cfg.Query, path)
-		mux.Handle("/"+path, &templateHandler{query: cfg.Query, template: cfg.Template})
+		mux.Handle("/"+path, &templateHandler{query: cfg.Query, template: cfg.Template, client: apiClient})
 	}
 	return mux
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/sbaier1/prometheus-view-proxy
 go 1.15
 
 require (
-	github.com/prometheus/client_golang v1.8.0 // indirect
+	github.com/hashicorp/consul/api v1.3.0
+	github.com/prometheus/client_golang v1.8.0
 	golang.org/x/tools v0.0.0-20201208211828-de58e7c01d49 // indirect
 	golang.org/x/tools/gopls v0.0.0-20201208211828-de58e7c01d49 // indirect
 	gopkg.in/yaml.v2 v2.3.0
